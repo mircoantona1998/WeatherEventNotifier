e4105315026a0b3fc1e5eaa039e11624519255fc
Mirco Arcangelo Antona <mirco.antona@algowatt.com>
2024-02-04 23:26:40 +0100

    ok

--- SchedulerService/app/SchedulerService.py
+++ SchedulerService/app/SchedulerService.py
@@ -4,11 +4,13 @@ from Utils.Logger import Logger
 import inspect
 from datetime import datetime
 from DB.Session import Session as DBSession
+import time
 from DBUsers.Session import Session as DBusers
 if __name__ == "__main__":
     Logger().log_action(f"{str(datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S'))} - avvio Scheduler service  - {inspect.currentframe().f_globals['__file__']}")
     Configurations()
     DBusers.wait_for_sql_server()
+    time.sleep(10)
     DBSession.wait_for_mysql()
     ConsumerClass().run_request()
     
